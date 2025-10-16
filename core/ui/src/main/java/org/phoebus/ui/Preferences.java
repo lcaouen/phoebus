9bf4bc706eb02ad7fbf2975f9c2ac0e0d252a605
Rebecca Williams <rjw@observatorysciences.co.uk>
2025-10-16 14:53:49 +0100

    Allow users to define a custom CSS file that can override defaults
    Allows users to override the 'not_enabled' styling as well as others
    used by Phoebus.
    Introduces a new property that can be set to the path of the CSS file.

--- core/ui/src/main/java/org/phoebus/ui/Preferences.java
+++ core/ui/src/main/java/org/phoebus/ui/Preferences.java
@@ -95,10 +95,8 @@ public class Preferences
     @Preference public static boolean save_credentials;
     /** documentation_location */
     @Preference public static String documentation_location;
-    /** window_title_format **/
-    @Preference public static String window_title_format;
-    /** default_window_title **/
-    @Preference public static String default_window_title;
+    /** Custom stylings file */
+    @Preference public static String custom_css_styling;
 
     static
     {
